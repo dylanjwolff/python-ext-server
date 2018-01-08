f8c6553310312bb7c9a9eb1db21fa94ceff5172f
wolffdy <wolffdy0@gmail.com>
2018-01-07 20:51:32 -0500

    playing around with testing flask applications; added failing tests for framework of TDD

--- src/main/server.py
+++ src/main/server.py
@@ -1,3 +1,19 @@
-def exampleFn(a, b):
-    return a + b;
+""" Server module """
+import logging
+from flask import Flask
+from flask import jsonify
+from flask import request
 
+APP = Flask(__name__)
+
+APP.config['MONGO_DBNAME'] = 'restdb'
+APP.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+
+@APP.route('/', methods=['POST'])
+def add_base():
+    """ adds base POST method """
+    logging.info('adding POST')
+    return jsonify({'result' : 'yay'})
+
+if __name__ == '__main__':
+    APP.run(debug=True, host='0.0.0.0', port=80)
--- src/main/server_test.py
+++ src/main/server_test.py
@@ -1,10 +1,30 @@
+""" Tests for server """
 import unittest
+import logging
+import sys
+from flask import request
+from flask import jsonify
+
 import server
+class TestApiMethods(unittest.TestCase):
+    """ Test class for Server """
 
-class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        print('wtf')
+        logging.info("Setting up for tests")
 
-    def test_split(self):
-        self.assertEqual(2, server.exampleFn(1,1))
+    def test_post(self):
+        """ test the POST method """
+        with server.APP.test_request_context('/', method='POST'):
+            response = server.APP.dispatch_request()
+            self.assertEqual("fail", response)
+
+    def test_post_alt(self):
+        """ test the POST method alt"""
+        with server.APP.test_client() as app:
+            response = app.post('/', data="test")
+            self.assertEqual("fail", response)
+
 
 if __name__ == '__main__':
     unittest.main()
